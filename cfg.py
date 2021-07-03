feadf1282d53dd7ce55a8653d12eed98b7e077bb
tky_hm_xps13h <k_code@163.com>
2021-07-04 00:05:35 +0800

    [cfg] stronger progressive aug & mixup = 0.2

--- cfg.py
+++ cfg.py
@@ -2,6 +2,16 @@ import yaml
 from easydict import EasyDict
 
 
+def dfs(item):
+    if isinstance(item, dict):
+        for k, v in item.items():
+            if isinstance(v, list):
+                return item, k, v
+            if isinstance(v, dict):
+                return dfs(v)
+    return None, None, None
+
+
 def parse_cfg(cfg_path, world_size, rank, only_model=False):
     with open(cfg_path) as f:
         cfg = yaml.safe_load(f)
@@ -9,19 +19,24 @@ def parse_cfg(cfg_path, world_size, rank, only_model=False):
     if only_model:
         return cfg.model
     
-    cp, ls_k, ls = None, None, None
+    dist_d, dist_k, dist_v = None, None, None
     for comp_k, comp in cfg.items():
         if comp_k != 'data' and isinstance(comp, dict):
-            for k, v in comp.items():
-                if isinstance(v, list):
-                    assert len(v) == world_size
-                    cp, ls_k, ls = comp, k, v
+            d, k, v = dfs(comp)
+            if d is not None:
+                dist_d, dist_k, dist_v = d, k, v
+                break
+            # for k, v in comp.items():
+            #     if isinstance(v, list):
+            #         assert len(v) == world_size
+            #         dist_d, dist_k, dist_v = comp, k, v
     
-    if cp is not None:
-        cp[ls_k] = ls[rank]
-        cfg.train.descs = [f'[rk{rk:02d}: {ls_k}={ls[rk]}]' for rk in range(world_size)]
-        cfg.train.descs_key = ls_k
-        cfg.train.descs_val = cp[ls_k]
+    if dist_d is not None:
+        assert len(dist_v) == world_size
+        dist_d[dist_k] = dist_v[rank]
+        cfg.train.descs = [f'[rk{rk:02d}: {dist_k}={dist_v[rk]}]' for rk in range(world_size)]
+        cfg.train.descs_key = dist_k
+        cfg.train.descs_val = dist_v[rank]
     else:
         cfg.train.descs = None
     
