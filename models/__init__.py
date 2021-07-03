f254a73611193d10afec83248d0b7a3fa731ff59
tky_hm_xps13h <k_code@163.com>
2021-07-03 11:13:49 +0800

    [fix] add efficientnet_b3_k

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@ def build_model(model_cfg):
     name, kw = model_cfg.name, model_cfg.kwargs
     kw.update({'num_classes': NUM_CLASSES})
     model = {
-        'efficientnet_b3': efficientnet_b3
+        'efficientnet_b3': efficientnet_b3,
         'efficientnet_b3_k': efficientnet_b3_k,
     }[name](**kw).cuda()
     ema = EMA(model, model_cfg.ema_mom)
