ea1bd1464832bfc290f319d5e697e58f140065a4
tky_hm_xps13h <k_code@163.com>
2021-07-05 15:04:14 +0800

    [release] vgg: stable version

--- eval.py
+++ eval.py
@@ -19,13 +19,14 @@ def main():
     parser = argparse.ArgumentParser(description='IPCV homework set 3 -- scene cls task')
     parser.add_argument('--ckpt_path', type=str, required=False, default='./best_effnetb3_ckpt.pth')
     parser.add_argument('--cfg', type=str, required=False, default='exp/effnetb3_epoch400batch16_lr0.04wd1e-3/cfg.yaml')
+    parser.add_argument('--data_root', type=str, required=False, default='../data')
     args = parser.parse_args()
 
     model_cfg = parse_cfg(args.cfg, None, None, only_model=True)
     ema, model = build_model(model_cfg, cuda=cuda)
     model.load_state_dict(torch.load(args.ckpt_path, map_location='cpu'))
     
-    te_set = Scene15Set(root_dir_path='/mnt/lustre/tiankeyu/datasets/scene15', train=False, vgg='vgg' in model_cfg.name)
+    te_set = Scene15Set(root_dir_path=args.data_root, train=False, vgg='vgg' in model_cfg.name)
     te_loader = DataLoader(te_set, 64, shuffle=False, num_workers=4, pin_memory=True, drop_last=False)
     test_acc, test_loss = eval_model(te_loader, model, cuda=cuda)
     print(f'[eval] test_acc={test_acc:5.2f}, test_loss={test_loss:.2f}')
